315d9ea0f05bbfecb624e76e224d16cabae99bbb
Xi Pu <xp19@duke.edu>
2018-10-09 20:33:31 -0400

    working on setup ui

---
+++ app/src/main/java/somethingmonkey/hackduke/ScreenSlidePagerActivity.java
@@ -0,0 +1,24 @@
+package somethingmonkey.hackduke;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ScreenSlidePagerActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        setContentView(R.layout.gender);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+}
