36e49bc6ce96eac8da5fc4f7c61832c6fcc2765e
Xi Pu <xp19@duke.edu>
2018-10-12 23:21:36 -0400

    add navigationview listener

--- app/src/main/java/somethingmonkey/hackduke/MainScreenActivity.java
+++ app/src/main/java/somethingmonkey/hackduke/MainScreenActivity.java
@@ -1,14 +1,18 @@
 package somethingmonkey.hackduke;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainScreenActivity extends AppCompatActivity{
+public class MainScreenActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout mDrawerLayout;
 
@@ -16,7 +20,7 @@ public class MainScreenActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-
+        setNavigationViewListner();
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -33,8 +37,33 @@ public class MainScreenActivity extends AppCompatActivity{
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        switch (menuItem.getItemId()) {
+
+            case R.id.nav_home: {
+                //do somthing
+                break;
+            }
+        }
+        //close navigation drawer
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setNavigationViewListner() {
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+//    public void onClickHeader(View v){
+//
+//    }
 
 }
